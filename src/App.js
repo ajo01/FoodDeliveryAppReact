b050799a265ee1fa98d4f8dfe899dbe523fb23f2
ajo01 <amyjojjm2001@gmail.com>
2021-06-23 00:39:57 -0700

    cartcontext and cartprovider

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
-import Header from './components/Layout/Header'
-import Meals from './components/Meals/Meals'
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CardProvider";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false)
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShown(true)
-  }
+    setCartIsShown(true);
+  };
 
   const hideCartHandler = () => {
-    setCartIsShown(false)
-  }
+    setCartIsShown(false);
+  };
 
   return (
-    <Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-    <Header onShowCart={showCartHandler}/>
-    <main>
-    <Meals/>
-    </main>
-    </Fragment>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
