77aedcd3f7b1c2a5f587b025d654881e19618278
ajo01 <amyjojjm2001@gmail.com>
2021-06-23 00:16:02 -0700

    modal closes on overlay click

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
