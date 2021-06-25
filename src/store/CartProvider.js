2f77411061ad38a3206fbc4128b72aa0dc06a53d
ajo01 <amyjojjm2001@gmail.com>
2021-06-24 17:34:06 -0700

    cart shows single entry for same fooditem with multiple orders

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,33 +2,54 @@ import CartContext from "./cart-context";
 import { useReducer } from "react";
 
 const defaultCartState = {
-  items:[],
-  totalAmount:0
-}
+  items: [],
+  totalAmount: 0,
+};
 
 const cartReducer = (state, action) => {
-  if (action.type === 'ADD') {
-    //new arr
-    const updatedItems = state.items.concat(action.item)
-    const newTotalAmount = state.totalAmount + action.item.price * action.item.amount
-    return {
-      items:updatedItems,
-      totalAmount:newTotalAmount
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
     }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
-  return defaultCartState
-}
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-    const addItemHandler = item =>{
-      dispatchCartAction({type:'ADD', item})
-    }
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item });
+  };
 
-    const removeItemHandler = id =>{
-      dispatchCartAction({type:'ADD', id})
-    }
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "ADD", id });
+  };
 
   const cartContext = {
     items: cartState.items,
@@ -37,7 +58,11 @@ const CartProvider = (props) => {
     removeItem: removeItemHandler,
   };
 
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
